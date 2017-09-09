b89f46850ba0e2d3860e3d62e18bf5bde87892e5
h141dev <github@h141dev.net>
2017-09-10 05:39:04 +0900

    Create custom.php

---
+++ src/lang/ja/custom.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    //Welcome Page
+    'login' => 'ログイン',
+    'register' => '登録',
+
+    //LogIn Page
+    'sign_in' => 'サインイン',
+    'email' => 'メールアドレス',
+    'password' => 'パスワード',
+
+    //Register
+    'new_register' => '新規登録',
+    'repeat_password' => 'パスワード再入力',
+
+    //Reset Password
+    'reset_password' => 'パスワード再設定',
+    'reset_password_button_label' => 'パスワード再設定用のリンク送信',
+
+    //Miscellaneous
+    'name' => '名前',
+    'dashboard' => 'ダッシュボード',
+
+    //Sidebar
+    'main_navigation' => 'メインナビゲーション'
+];
